88c08ba4ec8b5282150933d555b26cbe4e7599b4
Chris Pennington <cpenn368@gmail.com>
2025-08-26 16:37:20 -0600

    update var call

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import http from "node:http";
 import { Resend } from "resend";
 
-const resend = new Resend("${{RESEND_API_KEY}}");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const server = http.createServer(async (req, res) => {
   const { data, error } = await resend.emails.send({
